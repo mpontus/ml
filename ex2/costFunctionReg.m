636e12eba48dc6f97304893afbeb61eb51eef4e0
Michael Pontus <m.pontus@gmail.com>
2017-03-05 15:20:05 +0300

    Regularized Logistic Regression Gradient

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -19,6 +19,8 @@ grad = zeros(size(theta));
 
 J = - (1/m) * (y' * log(sigmoid(X * theta)) + ((1 - y)' * log(1 - sigmoid(X * theta)))) + lambda / (2 * m) * theta(2:end)' * theta(2:end);
 
+reg_theta = [0; theta(2:end)];
+grad = (1 / m) * (X' * (sigmoid(X * theta) - y)) + lambda / m * reg_theta;
 
 
 
