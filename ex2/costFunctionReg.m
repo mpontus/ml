45f884c98ab2ce2dfee9577a21e45c48c9e883a7
Michael Pontus <m.pontus@gmail.com>
2017-03-05 15:11:44 +0300

    Regularized Logistic Regression Cost

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,6 +17,7 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = - (1/m) * (y' * log(sigmoid(X * theta)) + ((1 - y)' * log(1 - sigmoid(X * theta)))) + lambda / (2 * m) * theta(2:end)' * theta(2:end);
 
 
 
