f2fb763bbe55e15ce46a0471e196129273f9cf9c
Michael Pontus <m.pontus@gmail.com>
2017-03-26 15:57:48 +0300

    Parameters (C, sigma) for Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,6 +23,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01 0.03 0.1 0.3 1 3 10 30];
+results = [];
+
+for C=steps
+  for sigma=steps
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    results = [results; C sigma error];
+  end
+end
+
+[x, ix] = min(results(:, 3));
+C = results(ix, 1);
+sigma = results(ix, 2);
 
 
 
